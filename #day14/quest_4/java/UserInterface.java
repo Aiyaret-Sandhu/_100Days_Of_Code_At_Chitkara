d38a390448f7085aecef7328f3f7296f29c613ba
= <arshsandhuprofessional@gmail.com>
2023-11-16 16:00:46 +0530

    question 4

---
+++ #day14/quest_4/java/UserInterface.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+// import java.lang.Integer;
+
+public class UserInterface {
+    private Container first;
+    private Container second;
+    private Scanner scanner;
+
+    public UserInterface(Scanner scanner) {
+        this.first = new Container();
+        this.second = new Container();
+        this.scanner = scanner;
+    }
+
+    public void start() {
+        while(true) {
+            System.out.println("First: " + this.first.toString() + "\nSecond: " + this.second.toString());
+
+            String input = scanner.nextLine();
+            if (input.equals("quit")) {
+                break;
+            }
+
+            String[] parts = input.split(" ");
+
+            String command = parts[0];
+            int amount = Integer.valueOf(parts[1]);
+
+            switch(command) {
+                case "add":
+                    this.first.add(amount);
+                    break;
+                
+                case "move":
+                    int present = this.first.contains();
+                    if (present >= amount) {
+                        this.first.remove(amount);
+                        this.second.add(amount);   
+                    } else {
+                        this.second.add(present);
+                        this.first.remove(present);
+                    }
+                    
+                    break;
+                
+                case "remove" :
+                    this.second.remove(amount);
+                    break;
+
+                default :
+                    System.out.println("Invalid Command");
+                    break;
+            }
+        }
+    }
+}
