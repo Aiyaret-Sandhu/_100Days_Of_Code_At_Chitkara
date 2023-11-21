7f0439586506f2741725d242ade3ece9a9eaed49
= <arshsandhuprofessional@gmail.com>
2023-11-21 15:50:42 +0530

    quest_5

---
+++ #day19/index5.py
@@ -0,0 +1,11 @@
+# # lst = [2**i for i in range(13)]
+# # print(lst) 
+
+# str1="John, Mohan, Aryan"
+# print(str1[-12 : -6])
+
+a={1:"A",2:"B",3:"C"}
+# for i,j in a.items():
+#     print(i,j,end=" ")
+
+print(a.items())
