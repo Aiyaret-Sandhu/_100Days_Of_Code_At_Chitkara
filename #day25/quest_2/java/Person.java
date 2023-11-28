e64b354cb806d6f9946d04259b708ed39537072c
= <arshsandhuprofessional@gmail.com>
2023-11-28 18:49:56 +0530

    question 2

---
+++ #day25/quest_2/java/Person.java
@@ -0,0 +1,21 @@
+public class Person {
+    private String name;
+    private String address;
+
+    public Person(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+    @Override
+    public String toString() {
+        return this.name + "\n  " + this.address;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
